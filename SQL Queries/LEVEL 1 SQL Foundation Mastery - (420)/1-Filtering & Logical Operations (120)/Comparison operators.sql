1e68eb255dd3ae4143616b41c8c9b749c7f216b3
Prafull Wahatule <prafullwahatule@gmail.com>
2025-12-21 19:30:56 +0530

    Solve QL Comparison Operators Practice

--- SQL Queries/LEVEL 1 SQL Foundation Mastery - (420)/1-Filtering & Logical Operations (120)/Comparison operators.sql
+++ SQL Queries/LEVEL 1 SQL Foundation Mastery - (420)/1-Filtering & Logical Operations (120)/Comparison operators.sql
@@ -4,41 +4,81 @@
 -- =============================================
 
 -- 1. Find all products whose UnitPrice > 20
+SELECT * FROM products
+WHERE UnitPrice > 20;
 
 -- 2. List all customers who are not from Germany
+SELECT * FROM customers 
+WHERE City != "Germany";
 
 -- 3. Find employees who were born before 1960
+SELECT * FROM employees
+WHERE YEAR(BirthDate) < 1960;
 
 -- 4. Retrieve orders where the ShipperID = 3
+SELECT * FROM orders
+WHERE ShipperID = 3;
 
 -- 5. Show all products where UnitsInStock = 0
+SELECT * FROM products
+WHERE UnitsInStock = 0;
 
 -- 6. Find suppliers where City = 'Tokyo'
+SELECT * FROM suppliers 
+WHERE City = "Tokyo";
 
 -- 7. List products where the ReorderLevel <= 10
+SELECT * FROM products
+WHERE ReorderLevel <= 10;
 
 -- 8. Find customers whose PostalCode >= '5000'
+SELECT * FROM customers
+WHERE PostalCode >= 5000;
 
 -- 9. Show orders placed by EmployeeID != 3
+SELECT * FROM orders
+WHERE EmployeeID != 3;
 
 -- 10. Find all categories where CategoryID > 5
+SELECT * FROM categories 
+WHERE CategoryID > 5;
 
 -- 11. Fetch order details where Quantity >= 20
+SELECT * FROM order_details 
+WHERE Quantity >= 20;
 
 -- 12. Retrieve products where Discontinued = 1
+SELECT * FROM products 
+WHERE Discontinued = 1;
 
 -- 13. Find suppliers not located in the USA
+SELECT * FROM suppliers
+WHERE Country != "USA";
 
 -- 14. List all products where CategoryID = 1
+SELECT * FROM products 
+WHERE CategoryID = 1;
 
 -- 15. Show employees where LastName < 'M'
+SELECT * FROM employees 
+WHERE LastName < "M";
 
 -- 16. Find customers where CustomerID > 50
+SELECT * FROM customers
+WHERE CustomerID > 50;
 
 -- 17. List all orders placed before '1996-07-10'
+SELECT * FROM orders
+WHERE OrderDate < '1996-07-10';
 
 -- 18. Show order details of products where ProductID <= 10
+SELECT * FROM order_details 
+WHERE ProductID <= 10;
 
 -- 19. Find suppliers whose SupplierID >= 20
+SELECT * FROM suppliers 
+WHERE SupplierID >= 20;
 
 -- 20. Retrieve products where UnitsOnOrder != 0
+SELECT * FROM products
+WHERE UnitsOnOrder != 0;
